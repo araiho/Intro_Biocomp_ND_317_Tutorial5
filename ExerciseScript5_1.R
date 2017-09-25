1786079d33fa150813516e40200ab618a3524795
Kyle Dubiak <kdubiak@nd.edu>
2017-09-25 13:38:22 -0400

    Cleaned up original script a bit

--- ExerciseScript5_1.R
+++ ExerciseScript5_1.R
@@ -1,12 +1,10 @@
 #This script will give the unique gender/years experience combinations from wages.csv
 wages<-read.csv(file="wages.csv",header=TRUE,stringsAsFactors = FALSE)
 females<-wages[wages$gender=="female",]
-orderedyears<-order(females$yearsExperience)
-orderedfemales<-females[orderedyears,1:2 ]
+orderedfemales<-females[order(females$yearsExperience),1:2]
 uniquefemales<-unique(orderedfemales,incomparables=FALSE)
 males<-wages[wages$gender=="male", ]
-orderedyearsm<-order(males$yearsExperience)
-orderedmales<-males[orderedyearsm,1:2]
+orderedmales<-males[order(males$yearsExperience),1:2]
 uniquemales<-unique(orderedmales,incomparables = FALSE)
 totalunique<-rbind(uniquefemales,uniquemales)
 write.table(totalunique,file="totalunique.txt")
