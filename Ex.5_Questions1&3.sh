7b173b199b03d4067fb1f50330fc1aea9836f853
Lindsay <lserene@nd.edu>
2017-09-27 17:52:46 -0400

    R Scripts for Exercise 5, Questons 1 and 3

---
+++ Ex.5_Questions1&3.sh
@@ -0,0 +1,40 @@
+#Exercise5_Question1
+
+# Set working directory
+setwd("~/Desktop/data-shell/Exercise5/Intro_Biocomp_ND_317_Tutorial5/")
+x = read.csv(file = "wages.csv")
+
+#Alternatively sort by gender and then years experience in one function:
+ordered_list <- x[order(x$gender, x$yearsExperience), c(1,2)]
+
+#Write unique gender-yearsExperience combinations to a text file called "Exercise5_Challenge1.txt"
+write.table(unique(ordered_list), "Exercise5_Challenge1.txt", quote = FALSE, row.names = FALSE)
+
+
+
+
+#Exercise5_Question3
+
+# Set working directory
+setwd("~/Desktop/data-shell/Exercise5/Intro_Biocomp_ND_317_Tutorial5/")
+
+#Read file
+x = read.csv(file = "wages.csv")
+
+#Sort by years schooling and then wage in one function:
+School.Effect <- x[order(x$yearsSchool, x$wage), c(3,4)]
+
+#Subset 12 years of schooling 
+Twelve <- subset(School.Effect, yearsSchool == "12", c(1,2))
+
+#Subset 16 years of schooling 
+Sixteen <- subset(School.Effect, yearsSchool == "16", c(1,2))
+
+#List minimum wage from 12 years subset
+MinWage12 <- head(Twelve, n=1)
+print(paste0("Min. wage with 12 years school: ", MinWage12$wage))
+
+
+#List minimum wage from 16 years subset
+MinWage16 <- head(Sixteen, n=1)
+print(paste0("Min. wage with 16 years school: ", MinWage16$wage))
