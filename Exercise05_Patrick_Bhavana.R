fc3cef0d30b49a7065f0a117445a0821ae04a295
Bhavana Palakurthi <bpalakur@nd.edu>
2017-09-27 02:50:30 +0100

    Exercise05_Patrick_Bhavana.R

---
+++ Exercise05_Patrick_Bhavana.R
@@ -0,0 +1,25 @@
+#set working directory
+setwd("C:/cygwin64/home/bhava/data-shell/molecules/Intro_Biocomp_ND_317_Tutorial5")
+#Load wages.csv file
+wages=read.csv(file="wages.csv",header=TRUE,stringsAsFactors=FALSE)
+#write unique combinations of gender and yearsExperience
+wages3=unique(wages[,1:2])
+#write the file to sort the yearsExperience of gender
+wages5=wages3[order( wages3[,2] ),]
+#print the lowest and highest earner
+wageshighestlowest=wages[order(wages[,4]),]
+head(wageshighestlowest, n=1)
+tail(wageshighestlowest, n=1)
+#print the topten earning females
+#Script for returning top ten earners as topten
+topten=head(wageshighestlowest, n=10)
+#Script for returning females in topten earners as toptenfemales
+toptenfemales=topten[topten[,1]=="female",]
+#Return the row with 12 years of experience with minimum wage
+tail(head(wageshighestlowest, n=3), n=1)
+#print the wages for 16yrs
+head(tail(yrschool, n=16), n=1)[,4]
+#print the min wages for 12yrs
+tail(head(wageshighestlowest, n=3), n=1)[,4]
+#print the difference in min wages for 16 yrs vs 12 yrs
+minwage=head(tail(yrschool, n=16), n=1)[,4] - tail(head(wageshighestlowest, n=3), n=1)[,4]
